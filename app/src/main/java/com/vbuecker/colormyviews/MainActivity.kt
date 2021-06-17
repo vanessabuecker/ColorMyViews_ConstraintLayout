4f48a791e76a6184d396ec834adb5fc1b0bdebe7
Vanessa Buecker <v.buecker@rocketmail.com>
2021-06-16 22:43:35 -0300

    "add buttons"

--- app/src/main/java/com/vbuecker/colormyviews/MainActivity.kt
+++ app/src/main/java/com/vbuecker/colormyviews/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.vbuecker.colormyviews
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.vbuecker.colormyviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val buttonGreen = binding.buttonGreen
+
+        buttonGreen.setOnClickListener {
+            binding.boxOne.setBackgroundColor(Color.GREEN)
+        }
+
+        binding.buttonRed.setOnClickListener {
+            binding.boxFourText.setBackgroundColor(Color.RED)
+        }
+
+        binding.buttonYellow.setOnClickListener {
+            binding.boxTwoText.setBackgroundColor(Color.YELLOW)
+            binding.boxTwoText.setTextColor(Color.BLACK)
+        }
     }
+
 }
